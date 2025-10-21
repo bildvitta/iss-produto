11de3533bcc14a3a255f2446aaad8a07eddd7a6c
Fábio Martins <fabiohbmartins@gmail.com>
2025-10-21 17:36:14 -0300

    Adjustments

--- src/Enums/CreditPolicy/Parameter/Type.php
+++ src/Enums/CreditPolicy/Parameter/Type.php
@@ -5,11 +5,8 @@ namespace Bildvitta\IssProduto\Enums\CreditPolicy\Parameter;
 enum Type: string
 {
     case PRO_SOLUTO = 'pro_soluto';
-
     case AFTER_KEY_DELIVERY = 'after_key_delivery';
-
     case INCOME = 'income';
-
     case INCOME_COMMITMENT = 'income_commitment';
 
     public function getLabel(): ?string
